1e1e069b11f4a235529ae4d700d581d01b4d8b11
JoaoLemes18 <joaolemes17@hotmail.com>
2023-08-15 15:25:08 -0400

    fix: fix the bugs

--- ChallengeII/app/(tabs)/_layout.tsx
+++ ChallengeII/app/(tabs)/_layout.tsx
@@ -1,14 +1,14 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable, useColorScheme } from "react-native";
 
-import Colors from '@/constants/Colors';
+import Colors from "@/constants/Colors";
 
 /**
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>["name"];
   color: string;
 }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
@@ -18,33 +18,34 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs 
+    <Tabs
       screenOptions={{
-        tabBarStyle: {display: 'none'},
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
+        tabBarStyle: { display: "none" },
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: "Tab One",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false
+          headerShown: false,
         }}
       />
       <Tabs.Screen
-        name="second"
+        name="secoundScreen"
         options={{
-          title: 'Tab Two',
+          title: "Tab Two",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false
+          headerShown: false,
         }}
       />
       <Tabs.Screen
-        name="third"
+        name="thirdScreen"
         options={{
-          title: 'Tab Three',
+          title: "Tab Three",
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerShown: false
+          headerShown: false,
         }}
       />
     </Tabs>
